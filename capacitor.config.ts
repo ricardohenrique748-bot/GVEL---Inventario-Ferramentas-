f324fc02a675398eacede495b291713e50b2bd12
Ricardo Luz <ricardo_h.16@hotmail.com>
2026-08-13 11:07:38 -0300

    chore: configure capacitor development server for local testing

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.gvel.inventarioferramentas',
   appName: 'Inventario Ferramentas - GV',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'http://localhost:3000',
+    cleartext: true
+  }
 };
 
 export default config;
